5d59692ece7c98a27fe81f750cc75c008eae66d7
iotool <roha.mbox@gmail.com>
2017-10-13 17:03:35 +0200

    dummy library and object library

---
+++ portable/sketchbook/privatelibrary1b/privatelibrary1.cpp
@@ -0,0 +1,11 @@
+// File: sketchbook/privatelibrary1/privatelibrary1.cpp
+// Info: step 1 - implementation of the application library
+
+#include <Arduino.h>
+#include "privatelibrary1.h"
+
+PrivateLibrary PLib;
+
+void PrivateLibrary::init() {}
+void PrivateLibrary::exec() {}
+
--- portable/sketchbook/privatelibrary1b/privatelibrary1b.ino
+++ portable/sketchbook/privatelibrary1b/privatelibrary1b.ino
@@ -1,1 +1,13 @@
+// File: sketchbook/privatelibrary1/privatelibrary1.ino
+// Info: step 1 - separate functionality into program and library 
 
+#include "privatelibrary1.h" // application library
+
+void setup() {
+  PLib.init();
+}
+
+void loop() {
+  PLib.exec();
+  delay(1000);
+}
