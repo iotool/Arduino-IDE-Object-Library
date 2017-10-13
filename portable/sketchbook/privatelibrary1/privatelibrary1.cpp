ef74e106b95dc5c26e7d991218fad8c1ac483e5e
iotool <roha.mbox@gmail.com>
2017-10-13 17:01:36 +0200

    master library for object code

---
+++ portable/sketchbook/privatelibrary1/privatelibrary1.cpp
@@ -0,0 +1,20 @@
+// File: sketchbook/privatelibrary1/privatelibrary1.cpp
+// Info: step 1 - implementation of the application library
+
+#include <Arduino.h>
+#include "privatelibrary1.h"
+
+PrivateLibrary PLib;
+
+void PrivateLibrary::init() {
+  if (!Serial) {
+    Serial.begin(11520);
+    delay(10);
+    Serial.println("*PLib: init()");
+  }
+}
+
+void PrivateLibrary::exec() {
+  Serial.println("*PLib: exec()");
+}
+
---
+++ portable/sketchbook/privatelibrary1/privatelibrary1.h
@@ -0,0 +1,15 @@
+// File: sketchbook/privatelibrary1/privatelibrary1.h
+// Info: step 1 - interface of the application library
+
+#ifndef PrivateLibrary_h_
+#define PrivateLibrary_h_
+
+class PrivateLibrary {
+  public:
+    void init();
+    void exec();
+};
+
+extern PrivateLibrary PLib;
+
+#endif /* PrivateLibrary_h_ */
