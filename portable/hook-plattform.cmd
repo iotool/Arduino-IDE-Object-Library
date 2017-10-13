a6583ae4f2696c1888ee18d060e6d4be214b9d51
iotool <roha.mbox@gmail.com>
2017-10-13 23:10:42 +0200

    add ide version

--- portable/hook-plattform.cmd
+++ portable/hook-plattform.cmd
@@ -6,6 +6,7 @@ set pbuild=%3
 set psource=%4
 set pproject=%5
 set pmcu=%6
+set pversion=%7
 
 if "%phook%"=="sketch.prebuild" (
   rem.
@@ -16,11 +17,11 @@ if "%phook%"=="sketch.prebuild" (
 if "%phook%"=="linking.prebuild" (
   rem.
   echo ### HOCK.LINKING.PREBUILD ### %psource%\*.cpp.o.hook-%pmcu%
-  xcopy %psource%\*.cpp.o.hook-%pmcu% %pbuild%\sketch /F /R /Y
+  xcopy %psource%\*.cpp.o.hook-%pmcu%-%pversion% %pbuild%\sketch /F /R /Y
   for %%G in ("%pbuild%\sketch\*.cpp.o") do (
-    if exist "%%G.hook-%pmcu%" (
-      echo ### HOOK %pmcu% ### %%G
-      xcopy %%G.hook-%pmcu% %%G /F /R /Y
+    if exist "%%G.hook-%pmcu%-%pversion%" (
+      echo ### HOOK %pmcu%-%pversion% ### %%G
+      xcopy %%G.hook-%pmcu%-%pversion% %%G /F /R /Y
     )
   )
 )
